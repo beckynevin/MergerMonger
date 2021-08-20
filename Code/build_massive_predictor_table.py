784d0ace7c426ee6b078b76414174e139342a54a
beckynevin <rebecca.nevin@cfa.harvard.edu>
2021-08-20 10:56:56 -0400

    Adding bar plot of reasons for failure and quantifying the reasons for fails

--- Code/build_massive_predictor_table.py
+++ Code/build_massive_predictor_table.py
@@ -57,7 +57,81 @@ df_all = df_all.drop_duplicates(subset=['ID'])
 print('length after dropping dups', len(df_all))
 
 
-df_all.to_csv('../Tables/SDSS_predictors_all.csv')
+#df_all.to_csv('../Tables/SDSS_predictors_all.txt',sep='\t')
 
 
 # Great, but how many are lost in the fails file and why do these fail?
+
+# The fails are stored somewhere different, first get the fails from the original run, then get the selection_2 fails
+prefix = '/Users/beckynevin/CfA_Code/Cannon/parallel_SDSS/'
+type_gal = 'DR12_predictors_MPI'
+df_fails_0 = pd.io.parsers.read_csv(prefix+'SDSS_'+str(type_gal)+'_fails_0.txt', sep='\t',dtype={'ID':str})
+df_fails_0.columns = ['ID','Reason']
+
+df_fails_1 = pd.io.parsers.read_csv(prefix+'SDSS_'+str(type_gal)+'_fails_1.txt', sep='\t',dtype={'ID':str})
+df_fails_1.columns = ['ID','Reason']
+
+df_fails_2 = pd.io.parsers.read_csv(prefix+'SDSS_'+str(type_gal)+'_fails_2.txt', sep='\t',dtype={'ID':str})
+df_fails_2.columns = ['ID','Reason']
+
+df_fails_3 = pd.io.parsers.read_csv(prefix+'SDSS_'+str(type_gal)+'_fails_3.txt', sep='\t',dtype={'ID':str})
+df_fails_3.columns = ['ID','Reason']
+
+df_fails_4 = pd.io.parsers.read_csv(prefix+'SDSS_'+str(type_gal)+'_fails_4.txt', sep='\t',dtype={'ID':str})
+df_fails_4.columns = ['ID','Reason']
+
+df_fails_5 = pd.io.parsers.read_csv(prefix+'SDSS_'+str(type_gal)+'_fails_5.txt', sep='\t',dtype={'ID':str})
+df_fails_5.columns = ['ID','Reason']
+
+df_fails_6 =  pd.io.parsers.read_csv('../Tables/SDSS_DR12_predictors_MPI_s2_fails_0.txt', sep='\t',dtype={'ID':str})
+df_fails_6.columns = ['ID','Reason']
+
+
+
+
+# Put these all together and delete repeats, then delete things that aren't in the overall photo...csv file
+df_fails_01 = df_fails_0.append(df_fails_1)
+df_fails_23 = df_fails_2.append(df_fails_3)
+df_fails_45 = df_fails_4.append(df_fails_5)
+df_fails_456 = df_fails_45.append(df_fails_6)
+
+df_fails_0123 = df_fails_01.append(df_fails_23)
+
+df_fails_all = df_fails_0123.append(df_fails_456)
+  
+
+# Now delete all duplicates in here
+# Now drow all duplicates of the first column
+df_fails_nodup = df_fails_all.drop_duplicates(subset=['ID'],keep='first')
+
+
+
+# Drop all rows that have the header:
+df_fails_all_nodup = df_fails_nodup[df_fails_nodup.ID != 'ID']
+print('len fails after dropping duplicate IDs and headers', len(df_fails_all_nodup))
+
+
+
+# Now drop anything that is not in the photo list
+cond = df_fails_all_nodup['ID'].isin(df_SQL['objID'])
+
+keep_fails = df_fails_all_nodup[cond]
+
+print('length of fails that you are keeping', len(keep_fails))
+
+# And now drop anything from fails that is already in df_all:
+cond = keep_fails['ID'].isin(df_all['ID'])
+
+
+double_keep_fails = keep_fails[~cond]
+print('len of fails after eliminating sources already measured', len(double_keep_fails))
+
+print('total length of SQL', len(df_SQL), 'length of fails and keeps', len(double_keep_fails),len(df_all),len(double_keep_fails)+len(df_all))
+print(double_keep_fails['Reason'].value_counts())
+
+double_keep_fails['Reason'].value_counts().plot(kind='bar')
+plt.tight_layout()
+plt.show()
+
+
+STOP
