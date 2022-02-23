6f05bdc9731a32443c341e041a306538344e7fd3
beckynevin <rebecca.nevin@cfa.harvard.edu>
2022-02-23 15:50:34 -0700

    Modified find_galaxy to also do segmap plotting and output a table with the results of the classification

--- Code/find_galaxy.py
+++ Code/find_galaxy.py
@@ -22,74 +22,12 @@ import matplotlib.pyplot as plt
 import scipy
 import scipy.stats
 from util_SDSS import SDSS_objid_to_values, download_galaxy
-
-# This is code from compare_mpmerg_to_full_population_CDF.py
-# But here for a list of p values
-def calculate_cdf(p_vals, p_list, percent):
-    # Define a histogram with spacing defined                                                                                                                
-    spacing = 1000 # this will be the histogram binning but also how finely sampled the CDF is                                                               
-    hist = np.histogram(p_vals, bins=spacing)
-
-    # Put this in continuous distribution form in order to calculate the CDF                                                                                 
-    hist_dist = scipy.stats.rv_histogram(hist)
-
-    # Find individual cdf values corresponding to a p_merg value                                                                                             
-    cdf_list = []
-    for p in p_list:
-        cdf_list.append(hist_dist.cdf(p))
-        
-
-    # Define the xs of this distribution                                                                                                                     
-    X = np.linspace(0, 1.0, spacing)
-
-    # Get all cdf values                                                                                                                                     
-    cdf_val = [hist_dist.cdf(x) for x in X]
-    # Find the x point at which the cdf value is 10% and 90% - 0.1 and 0.9 (can replace this with your own thresholds)                                       
-    idx_non, val_non = find_nearest(np.array(cdf_val), percent)
-    X_non = X[idx_non]
-
-    idx_merg, val_merg = find_nearest(np.array(cdf_val), 1 - percent)
-    X_merg =X[idx_merg]
-
-    print('p_merg value is ', X_non, 'when ',val_non,' of the full population has a lower p_merg value')
-    print('p_merg value is ', X_merg, 'when ',1-val_merg,' of the full population has a higher p_merg value')
+from util_smelter import get_predictors
+from util_LDA import calculate_cdf, find_nearest
 
 
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return idx, array[idx]
-
-
-
-
-# Step 1: import the SDSS galaxies
-merger_type = 'major_merger'
-plot = True
-size = 80
- 
-prefix = '/Users/rebeccanevin/Documents/CfA_Code/MergerMonger/Tables/'
-
-df_predictors = pd.io.parsers.read_csv(prefix+'SDSS_predictors_all_flags.txt', sep='\t')
-
-#df_predictors.columns = ['ID','Sep','Flux Ratio',  'Gini','M20','Concentration (C)','Asymmetry (A)','Clumpiness (S)','Sersic N','Shape Asymmetry (A_S)', 'Sersic AR', 'S/N', 'Sersic N Statmorph', 'A_S Statmorph']
-
-if len(df_predictors.columns) ==15: #then you have to delete the first column which is an empty index
-    df_predictors = df_predictors.iloc[: , 1:]
-  
-# Change the type of the ID in predictors:
-df_predictors = df_predictors.astype({'ID': 'int64'})#.dtypes                                                                                     
-
-
-# Step 2: import the probability values for these galaxies
-
-df_LDA = pd.io.parsers.read_csv(prefix+'LDA_out_all_SDSS_predictors_'+str(merger_type)+'_flags.txt', sep='\t')
-#print(df_LDA.columns, df_predictors.columns)
-df_LDA = df_LDA.astype({'ID': 'int64'})
-
-
-# Step 3: match from the list of IDs:
-# these are galaxyzoos that are classified as mergers
+# First, define the list of object IDs you want to run through the classification
+# These can either be from a list or imported from a table
 ID_list = [1237661465990201643,1237661951344312586,1237665530651345434,1237665571981689378,1237665571981689379]
 ID_list = [1237668649851945457, 1237668649851945458, 1237668569858375932,
  1237674649386811408, 1237648703503138948, 1237648703503138949,
@@ -108,6 +46,46 @@ ID_list = [1237668649851945457, 1237668649851945458, 1237668569858375932,
 # Alternately, just use your own lists of RAs and decs like so:
 #RA_list = [118.074345115, 119.617127519, 258.548475763, 241.150984777, 114.096383278, 189.213252539, 120.087417603, 205.690424787, 246.255977023, 251.335933471, 331.12290045, 205.753337262, 316.841308073, 127.170800449, 46.2941968423, 234.541843983, 319.193098655, 46.6649126989, 111.733682055, 322.213310988, 127.178093813, 123.820325773, 217.629970676, 262.399282723, 173.537567139, 215.017906947, 119.486337418, 123.330544326, 171.400653635, 321.00791167, 118.184152672, 119.182151794, 206.627529147, 247.159333462, 169.513447059, 206.007949798, 241.271446954, 258.82741019, 46.7170694138, 171.657261919, 255.029869751, 233.968342684, 47.1429889739, 61.4532623945, 131.725410562, 114.695391123, 118.855393765]
 #dec_list = [19.5950803976, 37.7866245591, 57.9760977732, 43.8797876113, 39.4382798053, 45.6511702678, 26.613527298, 24.5900537916, 24.2631556488, 42.757790046, 12.4426263785, 36.1656555546, 11.0664208661, 17.5814003238, -1.07547036487, 57.6036530229, 11.0437407875, 0.0619768826479, 41.0266910782, -1.07011823351, 45.7425550277, 46.0752533433, 52.7071590288, 54.4944236725, 49.2545623779, 47.1213302326, 39.9933651629, 46.1471565611, 54.3825744827, -0.366323610281, 45.949276388, 44.8567085266, 22.7060191923, 39.551266027, 45.1130288476, 25.9411986626, 45.4429921738, 57.6587701679, -0.896544995426, 51.5730412626, 37.8395021377, 57.9026365222, 0.55093690996, -6.32383706666, 25.3700887716, 29.8912832677, 39.1860944299]
+
+ID_list = [1237665179521187863,1237661069252231265,1237665329864114245]
+ID_list = [1237661360224403465,1237654954277404992,1237654954818535766,1237655108382949463,1237655498675060981,1237657874863947953,1237658297907478930,1237654605873348857,1237654606407073899,1237654654178623691]
+
+# This is what you're going to use as a suffix for the names of the saved images
+appellido = 'breakbrd_objids'
+
+# Get ID list instead from Dave's tables:
+fits_table = fits.open('../Tables/breakbrd_objids.fits')
+    #drpall-v3_1_1_objid.fits')
+
+ID_list = fits_table[1].data['objid']
+print('IDs', ID_list)
+
+
+
+# Define the sizes of the images and where your LDA and SDSS tables live:
+merger_type = 'major_merger'
+plot = True
+size = 80
+prefix = '/Users/rebeccanevin/Documents/CfA_Code/MergerMonger-dev/Tables/'
+
+# Now import the tables of predictor values and LDA values
+
+df_predictors = pd.io.parsers.read_csv(prefix+'SDSS_predictors_all_flags.txt', sep='\t')
+#df_predictors.columns = ['ID','Sep','Flux Ratio',  'Gini','M20','Concentration (C)','Asymmetry (A)','Clumpiness (S)','Sersic N','Shape Asymmetry (A_S)', 'Sersic AR', 'S/N', 'Sersic N Statmorph', 'A_S Statmorph']
+
+if len(df_predictors.columns) ==15: #then you have to delete the first column which is an empty index
+    df_predictors = df_predictors.iloc[: , 1:]
+  
+# Change the type of the ID in predictors:
+df_predictors = df_predictors.astype({'ID': 'int64'})#.dtypes                                                                                     
+
+
+# Import the probability values for all SDSS galaxies
+
+df_LDA = pd.io.parsers.read_csv(prefix+'LDA_out_all_SDSS_predictors_'+str(merger_type)+'_flags_leading_preds.txt', sep='\t')
+#print(df_LDA.columns, df_predictors.columns)
+df_LDA = df_LDA.astype({'ID': 'int64'})
+
 
 
 ra_dec_lookup = pd.read_csv('../Tables/five_sigma_detection_saturated_mode1_beckynevin.csv')
@@ -162,6 +140,10 @@ print('p_merg value is ', X_merg, 'when ',1-val_merg,' of the full population ha
 indices_preds = df_predictors.index
 
 
+if appelido == 'ABC': # This is for making the ABC plot in the paper
+    counter_ABC = 0
+    ABC = ['A','B','C']
+    gal_colors = ['#CE7DA5','#6DD3CE','#E59500']
 
 
 for i in range(len(ID_list)):
@@ -215,25 +197,149 @@ for i in range(len(ID_list)):
             img = download_galaxy(id, ra, dec, prefix+'../frames/', size)
 
             shape = np.shape(img)[0]
+
+            print('getting ready for flags')
+
+            flag = False
+            flag_name = []
+            if (df_predictors.values[where_predictors][11]==0) & (df_predictors.values[where_predictors][12]==0) & (df_predictors.values[where_predictors][13]==0):
+                flag_name.append('No flag')
+            else:
+                if df_predictors.values[where_predictors][11]==1:
+                    flag_name.append('low S/N')
+                if df_predictors.values[where_predictors][12]==1:
+                    flag_name.append('outlier predictor')
+                if df_predictors.values[where_predictors][13]==1:
+                    flag_name.append('segmap')
+                flag = True
+
+
             
             plt.clf()
             fig = plt.figure()
             ax = fig.add_subplot(111)
-            ax.imshow(abs(img), norm=matplotlib.colors.LogNorm())
+            ax.imshow(abs(img), norm=matplotlib.colors.LogNorm(vmax=10**5, vmin=10**(0.5)), cmap='afmhot', interpolation='None')
+
+            preds = get_predictors(id, img, prefix+'../', size)
+            try:
+                segmap = preds[1]
+            except TypeError:
+                continue
+            ax.contour(np.fliplr(np.rot90(preds[1])), levels=[0,1], colors='yellow')
+
+            if appelido == 'ABC':
+
+                ax.annotate(ABC[counter_ABC],xy=(0.7,0.05),xycoords='axes fraction',color=gal_colors[counter_ABC], size=100)
+                counter_ABC+=1
+
+            ax.annotate('Gini = '+str(round(df_predictors.values[where_predictors][2],2))+
+                r' M$_{20}$ = '+str(round(df_predictors.values[where_predictors][3],2))+
+                '\nC = '+str(round(df_predictors.values[where_predictors][4],2))+
+                ' A = '+str(round(df_predictors.values[where_predictors][5],2))+
+                ' S = '+str(round(df_predictors.values[where_predictors][6],2))+
+                '\nn = '+str(round(df_predictors.values[where_predictors][7],2))+
+                r' A$_S$ = '+str(round(df_predictors.values[where_predictors][8],2))+
+                '\n<S/N> = '+str(round(df_predictors.values[where_predictors][9],2)), 
+                xy=(0.03, 0.05),  xycoords='axes fraction',
+            xytext=(0.03, 0.05), textcoords='axes fraction',
+            bbox=dict(boxstyle="round", fc="0.9", alpha=0.5), color='black')
+        
+            
             ax.annotate('LD1 = '+str(round(df_LDA.values[where_LDA][0][3],2))+'\n$p_{\mathrm{merg}}$ = '+str(round(df_LDA.values[where_LDA][0][4],4))+'\nCDF = '+str(round(cdf,4)), xy=(0.03, 0.85),  xycoords='axes fraction',
             xytext=(0.03, 0.85), textcoords='axes fraction',
             bbox=dict(boxstyle="round", fc="0.9"), color='black')
-            if df_LDA.values[where_LDA][0][8] ==1 or df_LDA.values[where_LDA][0][9] ==1:
+
+            
+            if flag:
                 ax.annotate('Photometric Flag', xy=(0.8, 0.85),  xycoords='axes fraction',
             xytext=(0.8, 0.85), textcoords='axes fraction',
             bbox=dict(boxstyle="round", fc="0.9"), color='black')
+            
             ax.set_title('ObjID = '+str(id))
             ax.set_xticks([0, (shape - 1)/2, shape-1])
             ax.set_xticklabels([-size/2, 0, size/2])
             ax.set_yticks([0, (shape- 1)/2,shape-1])
             ax.set_yticklabels([-size/2, 0,size/2])
             ax.set_xlabel('Arcsec')
-            plt.savefig(prefix+'../Figures/ind_galaxies_classify/'+str(id)+'_'+str(merger_type)+'_gz_merg.png', dpi=1000)
+            plt.savefig(prefix+'../Figures/ind_galaxies_classify/classification_'+str(id)+'_'+str(merger_type)+'_'+appelido+'.png', dpi=1000)
+
+            # Make a second plot that includes the segmentation map
+            center_val = segmap[int(np.shape(segmap)[0]/2),int(np.shape(segmap)[1]/2)]
+            # Need to make a list of all points on the edge of the map
+            edges=np.concatenate([segmap[0,:-1], segmap[:-1,-1], segmap[-1,::-1], segmap[-2:0:-1,0]])
+
+            '''
+            coord_list = []
+            for p in range(np.shape(segmap)[0]):
+                coord_list.append((0,p))
+                coord_list.append((np.shape(segmap)[0]-1,p))
+                coord_list.append((p,0))
+                coord_list.append((p,np.shape(segmap)[0]-1))
+
+            print('coordinate 0', coord_list[0], coord_list[0])
+            print(segmap[0,0], segmap[(0,0)])
+            print('play with this', segmap[0:10,0:10])
+            print('shape of coord list', np.shape(coord_list))
+            print('shape of thing youre doing .any to ', np.shape(segmap[coord_list]))
+            '''
+            #print(coord_list)
+            
+
+            '''
+            if edges.any():
+                flag = 2
+                flag_name = 'On edge'
+            if center_val == False:
+                flag = 1
+                flag_name = 'Not centered'
+            if (center_val == False) & edges.any():
+                flag = 3
+                flag_name = 'Not centered + on edge'
+            # 1 is not centered, 2 is on edge, 3 is both
+            #if flag > 0: # this means its been flagged for something
+            '''
+
+
+            fig = plt.figure()
+            ax1 = fig.add_subplot(121)
+            im1 = ax1.imshow(abs(np.fliplr(np.rot90(preds[0]))), norm=matplotlib.colors.LogNorm())#, origin = 'lower'
+            ax1.set_title('Image, flag = \n'+str(flag_name))
+            ax1.annotate('LD1 = '+str(round(df_LDA.values[where_LDA][0][3],2))+'\n$p_{\mathrm{merg}}$ = '+str(round(df_LDA.values[where_LDA][0][4],4))+
+                '\nCDF = '+str(round(cdf,4))+
+                '\n'+str(df_LDA.values[where_LDA][0][6])+'\n'+str(df_LDA.values[where_LDA][0][8])+'\n'+str(df_LDA.values[where_LDA][0][10]), 
+                xy=(0.03, 0.7),  xycoords='axes fraction',
+            xytext=(0.03, 0.7), textcoords='axes fraction',
+            bbox=dict(boxstyle="round", fc="0.9", alpha=0.5), color='black')
+
+            if flag:
+                ax1.annotate('Photometric Flag', xy=(0.5, 0.85),  xycoords='axes fraction',
+            xytext=(0.5, 0.85), textcoords='axes fraction',
+            bbox=dict(boxstyle="round", fc="0.9"), color='black')
+
+            ax2 = fig.add_subplot(122)
+            im2 = ax2.imshow(np.fliplr(np.rot90(preds[1])))
+            ax2.scatter(np.shape(segmap)[0]/2,np.shape(segmap)[0]/2, color='red')
+            ax2.set_title('Segmap, <S/N> = '+str(round(preds[10],2)))
+            ax2.annotate('Gini = '+str(round(df_predictors.values[where_predictors][3],2))+
+                ' M20 = '+str(round(df_predictors.values[where_predictors][4],2))+
+                '\nC = '+str(round(df_predictors.values[where_predictors][5],2))+
+                '\nA = '+str(round(df_predictors.values[where_predictors][6],2))+
+                '\nS = '+str(round(df_predictors.values[where_predictors][7],2))+
+                '\nn = '+str(round(df_predictors.values[where_predictors][8],2))+
+                '\nA_S = '+str(round(df_predictors.values[where_predictors][9],2)), 
+                xy=(0.03, 0.55),  xycoords='axes fraction',
+            xytext=(0.03, 0.55), textcoords='axes fraction',
+            bbox=dict(boxstyle="round", fc="0.9", alpha=0.5), color='black')
+            ax1.set_xticks([0, (shape - 1)/2, shape-1])
+            ax1.set_xticklabels([-size/2, 0, size/2])
+            ax1.set_yticks([0, (shape- 1)/2,shape-1])
+            ax1.set_yticklabels([-size/2, 0,size/2])
+            ax1.set_xlabel('Arcsec')
+            plt.savefig(prefix+'../Figures/ind_galaxies_classify/statmorph_check_'+str(id)+'_'+str(merger_type)+'_'+appellido+'.png', dpi=1000)
+            if flag:
+                flag_list.append(1)
+            else:
+                flag_list.append(0)
             
         index_save_LDA.append(where_LDA[0])
         index_save_predictors.append(where_predictors)
